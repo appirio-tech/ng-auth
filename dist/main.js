5ad822bfd18306c362784778dc11cd4e0776437f
Paula Romero <promero@appirio.com>
2015-07-06 12:17:39 -0700

    changed http request

--- dist/main.js
+++ dist/main.js
@@ -321,7 +321,7 @@
             }
           }
         };
-        resource = UserV3APIService.save(userParams);
+        resource = UserV3APIService.post(userParams);
         resource.$promise.then(function(response) {
           return typeof callback === "function" ? callback(response) : void 0;
         });
